eed53dbdd6a07a90476b723f3dd52ecf01b999df
mskatz <zur.katarzyna@gmail.com>
2018-01-21 10:55:33 +0100

    Added comments

--- app/src/main/java/com/example/mskatz/scorekeeper/MainActivity.java
+++ app/src/main/java/com/example/mskatz/scorekeeper/MainActivity.java
@@ -7,9 +7,13 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    //Stores an information about Deadpool's choice
     String choiceD = null;
+    //Stores an information about Spider-Man's choice
     String choiceS = null;
+    //general Spider-Man's score
     int scoreS = 0;
+    //general Deadpool's score
     int scoreD = 0;
 
     @Override
@@ -18,30 +22,37 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    //Chooses rock in a rock-paper-scissors game for Deadpool
     public void chooseRockD(View v) {
         choiceD = "rock";
     }
 
+    //Chooses paper in a rock-paper-scissors game for Deadpool
     public void choosePaperD(View v) {
         choiceD = "paper";
     }
 
+    //Chooses scissors in a rock-paper-scissors game for Deadpool
     public void chooseScissorsD(View v) {
         choiceD = "scissors";
     }
 
+    //Chooses rock in a rock-paper-scissors game for Spider-Man
     public void chooseRockS(View v) {
         choiceS = "rock";
     }
 
+    //Chooses paper in a rock-paper-scissors game for Spider-Man
     public void choosePaperS(View v) {
         choiceS = "paper";
     }
 
+    //Chooses scissors in a rock-paper-scissors game for Spider-Man
     public void chooseScissorsS(View v) {
         choiceS = "scissors";
     }
 
+    //Checks who won in a rock-paper-scissors game
     public void checkWhoWin(View v) {
 
         String spiderManWin = "Point to Spider-Man";
@@ -80,16 +91,19 @@ public class MainActivity extends AppCompatActivity {
         choiceS = null;
     }
 
+    //Adds one point to general Deadpool's score
     public void addScoreD(int score) {
         scoreD = scoreD + 1;
         displayScoreD(scoreD);
     }
 
+    //Adds one point to general Spider-Man's score
     public void addScoreS(int score) {
         scoreS = scoreS + 1;
         displayScoreS(scoreS);
     }
 
+    //Resets the score to 0 for two teams
     public void resetScore(View v) {
         scoreS = 0;
         scoreD = 0;
@@ -100,16 +114,19 @@ public class MainActivity extends AppCompatActivity {
         displayInfo("Let's start...");
     }
 
+    //Displays Deadpool's score
     public void displayScoreD(int score) {
         TextView scoreView = findViewById(R.id.score_Deadpool);
         scoreView.setText(String.valueOf(score));
     }
 
+    //Displays Spider-Man's score
     public void displayScoreS(int score) {
         TextView scoreView = findViewById(R.id.score_SpiderMan);
         scoreView.setText(String.valueOf(score));
     }
 
+    //Displays messages during the game
     public void displayInfo(String info) {
         TextView infoView = findViewById(R.id.info);
         infoView.setText(info);
